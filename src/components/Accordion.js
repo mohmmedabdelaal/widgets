388fff09bada6fcfafa6804a4c99baed3df41dec
Moelaal <medo.elaal@gmail.com>
2022-07-09 22:40:04 +0200

    searching with axios

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,29 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Accordion = ({ items }) => {
-  const onTitleClick = (indx) => {
-    console.log(`title ${indx} clicked`);
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const onTitleClick = (index) => {
+    setActiveIndex(index);
   };
-  return (
-    <div className="ui styled accordion">
-      {items.map((item, index) => {
-        return (
-          <React.Fragment key={index}>
-            <div className="title active">
-              <i
-                className="dropdown icon"
-                onClick={() => onTitleClick(index)}
-              ></i>
-              {item.title}
-            </div>
-            <div className="content active">
-              <p>{item.content}</p>
-            </div>
-          </React.Fragment>
-        );
-      })}
-    </div>
-  );
+
+  const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? 'active' : '';
+
+    return (
+      <React.Fragment key={item.title}>
+        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className={`content ${active}`}>
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
+
+  return <div className="ui styled accordion">{renderedItems}</div>;
 };
 
 export default Accordion;
