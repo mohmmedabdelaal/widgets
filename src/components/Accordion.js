80c77b287b43444419a5c2bef5ac7b53c81d27ee
Moelaal <medo.elaal@gmail.com>
2022-07-09 21:08:41 +0200

    check every title

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
 const Accordion = ({ items }) => {
+  const onTitleClick = (indx) => {
+    console.log(`title ${indx} clicked`);
+  };
   return (
     <div className="ui styled accordion">
-      {items.map((item) => {
+      {items.map((item, index) => {
         return (
-          <React.Fragment key={item.title}>
+          <React.Fragment key={index}>
             <div className="title active">
-              <i className="dropdown icon"></i>
+              <i
+                className="dropdown icon"
+                onClick={() => onTitleClick(index)}
+              ></i>
               {item.title}
             </div>
             <div className="content active">
