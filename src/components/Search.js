9752040463634bffb5efb1331c364330843754b3
Moelaal <medo.elaal@gmail.com>
2022-07-10 16:34:15 +0200

    adding cors

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,25 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const url = 'https://en.wikipedia.org/w/api.php';
-
 const Search = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('programming');
+  const [results, setResults] = useState([]);
 
-  const searchWiki = async () => {
-    await axios.get(url, {
-      params: {
-        action: 'query',
-        list: 'search',
-        origin: '*',
-        format: 'json',
-        srsearch: term,
-      },
-    });
-  };
   useEffect(() => {
-    searchWiki();
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+
+      setResults(data.query.search);
+    };
+
+    if (term) {
+      search();
+    }
   }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -33,6 +56,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
